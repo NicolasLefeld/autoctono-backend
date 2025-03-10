bdf87de081e80a104d2e64c8efc8639dcef1e2b4
Nicolas Lefeld <nicolefeld@7r1ck.com>
2025-03-10 13:47:55 -0300

    Refactor database configuration to use pg-connection-string for improved connection handling

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,18 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import { parse } from "pg-connection-string";
 
 dotenv.config();
 
+const config = parse(process.env.DATABASE_URL as string);
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD as string,
+  config.database as string,
+  config.user as string,
+  config.password as string,
   {
-    host: process.env.DB_HOST,
+    host: config.host ? config.host : "localhost",
+    port: Number(config.port),
     dialect: "postgres",
     logging: console.log,
     dialectOptions: {
